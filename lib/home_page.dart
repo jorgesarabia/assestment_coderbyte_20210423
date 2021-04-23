e3c67a8a1fa83f7e729130304f2935f204659be6
Jorge Sarabia <jorge@sarabiajor.ge>
2021-04-23 12:04:08 -0300

    Refactoring

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assestment 23-04-2021'),
+      ),
+      body: Container(),
+    );
+  }
+}
